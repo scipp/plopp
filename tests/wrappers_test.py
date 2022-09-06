aaaa154ede13bdcb31c3f5a4c02964e0469b37ae
Neil Vaytet <neil.vaytet@esss.se>
2022-09-06 11:53:20 +0200

    update tests

--- tests/wrappers_test.py
+++ tests/wrappers_test.py
@@ -8,36 +8,51 @@ import plopp as pp
 
 
 def test_plot_ndarray():
-    pp.plot(np.arange(50.))._to_widget()
+    pp.plot(np.arange(50.))
 
 
 def test_plot_variable():
-    pp.plot(sc.arange('x', 50.))._to_widget()
+    pp.plot(sc.arange('x', 50.))
 
 
 def test_plot_data_array():
-    pp.plot(make_dense_data_array(ndim=1))._to_widget()
+    pp.plot(make_dense_data_array(ndim=1))
     da = make_dense_data_array(ndim=2)
-    pp.plot(da)._to_widget()
+    pp.plot(da)
+    pp.plot(da)
 
 
 def test_plot_data_array_missing_coords():
-    pp.plot(sc.data.table_xyz(100))._to_widget()
+    da = sc.data.table_xyz(100)
+    pp.plot(da)
+    assert 'row' not in da.coords
 
 
 def test_plot_dataset():
     ds = make_dense_dataset(ndim=1)
-    pp.plot(ds)._to_widget()
+    pp.plot(ds)
 
 
 def test_plot_dict_of_ndarrays():
-    pp.plot({'a': np.arange(50.), 'b': np.arange(60.)})._to_widget()
+    pp.plot({'a': np.arange(50.), 'b': np.arange(60.)})
 
 
 def test_plot_dict_of_variables():
-    pp.plot({'a': sc.arange('x', 50.), 'b': sc.arange('x', 60.)})._to_widget()
+    pp.plot({'a': sc.arange('x', 50.), 'b': sc.arange('x', 60.)})
 
 
 def test_plot_dict_of_data_arrays():
     ds = make_dense_dataset(ndim=1)
-    pp.plot({'a': ds['a'], 'b': ds['b']})._to_widget()
+    pp.plot({'a': ds['a'], 'b': ds['b']})
+
+
+def test_plot_data_array_2d_with_one_missing_coord_and_binedges():
+    da = sc.data.table_xyz(100).bin(x=10, y=12).bins.mean()
+    del da.coords['x']
+    pp.plot(da)
+
+
+def test_plot_coord_with_no_unit():
+    da = sc.data.table_xyz(100).bin(x=10).bins.mean()
+    da.coords['x'].unit = None
+    pp.plot(da)
