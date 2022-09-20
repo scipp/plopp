e84eef90792462a1edae292452c62a25705cb3b7
Neil Vaytet <neil.vaytet@esss.se>
2022-09-20 17:15:27 +0200

    add line tests

---
+++ tests/graphics/line_test.py
@@ -0,0 +1,86 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+
+from plopp.data import dense_data_array
+from plopp.graphics.line import Line
+import matplotlib.pyplot as plt
+import numpy as np
+import scipp as sc
+
+
+def make_axes():
+    fig, ax = plt.subplots()
+    return ax
+
+
+def test_line_creation():
+    da = dense_data_array(ndim=1, unit='K')
+    line = Line(ax=make_axes(), data=da)
+    assert line._unit == 'K'
+    assert line._dim == 'xx'
+    assert len(line._line.get_xdata()) == da.sizes['xx']
+    assert np.allclose(line._line.get_xdata(), da.meta['xx'].values)
+    assert np.allclose(line._line.get_ydata(), da.values)
+    assert line._error is None
+    assert not line._mask.get_visible()
+
+
+def test_line_creation_bin_edges():
+    da = dense_data_array(ndim=1, binedges=True)
+    line = Line(ax=make_axes(), data=da)
+    assert len(line._line.get_xdata()) == da.sizes['xx'] + 1
+
+
+def test_line_with_errorbars():
+    da = dense_data_array(ndim=1, with_variance=True)
+    line = Line(ax=make_axes(), data=da)
+    assert line._error.has_yerr
+    coll = line._error.get_children()[0]
+    x = np.array(coll.get_segments())[:, 0, 0]
+    y1 = np.array(coll.get_segments())[:, 0, 1]
+    y2 = np.array(coll.get_segments())[:, 1, 1]
+    assert np.allclose(x, da.meta['xx'].values)
+    assert np.allclose(y1, (da.data - sc.stddevs(da.data)).values)
+    assert np.allclose(y2, (da.data + sc.stddevs(da.data)).values)
+
+
+def test_line_hide_errorbars():
+    da = dense_data_array(ndim=1, with_variance=True)
+    line = Line(ax=make_axes(), data=da, errorbars=False)
+    assert line._error is None
+
+
+def test_line_with_mask():
+    da = dense_data_array(ndim=1, masks=True)
+    line = Line(ax=make_axes(), data=da)
+    assert line._mask.get_visible()
+
+
+def test_line_update():
+    da = dense_data_array(ndim=1)
+    line = Line(ax=make_axes(), data=da)
+    assert np.allclose(line._line.get_xdata(), da.meta['xx'].values)
+    assert np.allclose(line._line.get_ydata(), da.values)
+    line.update(da * 2.5)
+    assert np.allclose(line._line.get_xdata(), da.meta['xx'].values)
+    assert np.allclose(line._line.get_ydata(), da.values * 2.5)
+
+
+def test_line_update_with_errorbars():
+    da = dense_data_array(ndim=1, with_variance=True)
+    line = Line(ax=make_axes(), data=da)
+    coll = line._error.get_children()[0]
+    x = np.array(coll.get_segments())[:, 0, 0]
+    y1 = np.array(coll.get_segments())[:, 0, 1]
+    y2 = np.array(coll.get_segments())[:, 1, 1]
+    assert np.allclose(x, da.meta['xx'].values)
+    assert np.allclose(y1, (da.data - sc.stddevs(da.data)).values)
+    assert np.allclose(y2, (da.data + sc.stddevs(da.data)).values)
+    new_values = da * 2.5
+    new_values.variances = da.variances
+    line.update(new_values)
+    coll = line._error.get_children()[0]
+    y1 = np.array(coll.get_segments())[:, 0, 1]
+    y2 = np.array(coll.get_segments())[:, 1, 1]
+    assert np.allclose(y1, (da.data * 2.5 - sc.stddevs(da.data)).values)
+    assert np.allclose(y2, (da.data * 2.5 + sc.stddevs(da.data)).values)
