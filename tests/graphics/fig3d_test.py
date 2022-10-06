6e7f4d060016c358f56c0ba7a20d5902a66fdc80
Neil Vaytet <neil.vaytet@esss.se>
2022-10-06 16:53:18 +0200

    fix fig3d tests

--- tests/graphics/fig3d_test.py
+++ tests/graphics/fig3d_test.py
@@ -9,6 +9,12 @@ import scipp as sc
 from copy import copy
 
 
+def _make_limits():
+    return (sc.array(dims=['x'], values=[1.5, 3.5],
+                     unit='m'), sc.array(dims=['y'], values=[-5.0, 5.0], unit='m'),
+            sc.array(dims=['z'], values=[-0.1, 17.0], unit='m'))
+
+
 def test_creation():
     fig = Fig3d(figsize=(700, 450))
     assert len(fig._children) == 0
@@ -19,30 +25,9 @@ def test_creation():
     assert fig.outline is None
 
 
-def test_update_with_point_cloud():
-    fig = Fig3d(x='x', y='y', z='z')
-    assert len(fig._children) == 0
-    da = scatter_data()
-    key = 'scatter'
-    fig.update(da, key=key)
-    assert isinstance(fig._children[key], PointCloud)
-    assert sc.identical(fig._children[key]._data, da)
-    assert fig.outline is not None
-
-
-def test_create_with_node():
-    da = scatter_data()
-    fig = Fig3d(input_node(da), x='x', y='y', z='z')
-    assert len(fig._children) == 1
-    child = list(fig._children.values())[0]
-    assert isinstance(child, PointCloud)
-    assert sc.identical(child._data, da)
-    assert fig.outline is not None
-
-
 def test_camera_home():
-    da = scatter_data()
-    fig = Fig3d(input_node(da), x='x', y='y', z='z')
+    fig = Fig3d()
+    fig._update_camera(limits=_make_limits())
     original = copy(fig.camera.position)
     fig.camera.position = [10, 20, 30]
     fig.home()
@@ -50,8 +35,8 @@ def test_camera_home():
 
 
 def test_camera_x():
-    da = scatter_data()
-    fig = Fig3d(input_node(da), x='x', y='y', z='z')
+    fig = Fig3d()
+    fig._update_camera(limits=_make_limits())
     center = copy(fig.camera_backup["center"])
     fig.camera.position = [10, 20, 30]
     fig.camera_x_normal()
@@ -65,8 +50,8 @@ def test_camera_x():
 
 
 def test_camera_y():
-    da = scatter_data()
-    fig = Fig3d(input_node(da), x='x', y='y', z='z')
+    fig = Fig3d()
+    fig._update_camera(limits=_make_limits())
     center = copy(fig.camera_backup["center"])
     fig.camera.position = [10, 20, 30]
     fig.camera_y_normal()
@@ -80,8 +65,8 @@ def test_camera_y():
 
 
 def test_camera_z():
-    da = scatter_data()
-    fig = Fig3d(input_node(da), x='x', y='y', z='z')
+    fig = Fig3d()
+    fig._update_camera(limits=_make_limits())
     center = copy(fig.camera_backup["center"])
     fig.camera.position = [10, 20, 30]
     fig.camera_z_normal()
@@ -94,19 +79,8 @@ def test_camera_z():
     assert fig.camera.position[2] > center[2]
 
 
-def test_toggle_outline():
-    da = scatter_data()
-    fig = Fig3d(input_node(da), x='x', y='y', z='z')
-    assert fig.outline.visible
-    fig.toggle_outline()
-    assert not fig.outline.visible
-    fig.toggle_outline()
-    assert fig.outline.visible
-
-
 def test_toggle_axes_3d():
-    da = scatter_data()
-    fig = Fig3d(input_node(da), x='x', y='y', z='z')
+    fig = Fig3d()
     assert fig.axes_3d.visible
     fig.toggle_axes3d()
     assert not fig.axes_3d.visible
