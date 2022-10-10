5b3aa967d358926017321f63368db80aef4efc1e
Neil Vaytet <neil.vaytet@esss.se>
2022-10-10 20:27:34 +0200

    make a fig2d

---
+++ src/plopp/graphics/fig2d.py
@@ -0,0 +1,70 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+
+from ..core.utils import number_to_variable, name_with_unit
+from .fig import Figure
+from .color_mapper import ColorMapper
+from .io import fig_to_bytes
+from .mesh import Mesh
+from .line import Line
+
+import matplotlib.pyplot as plt
+import scipp as sc
+from typing import Any, Tuple
+
+
+class Figure2d(Figure):
+
+    def __init__(self, *args, **kwargs):
+
+        self.colormapper = ColorMapper()
+
+        super().__init__(*args, **kwargs)
+
+    def update(self, new_values: sc.DataArray, key: str, draw: bool = True):
+        """
+        Update image array with new values.
+        """
+        if new_values.ndim != 2:
+            raise ValueError("Figure can only be used to plot 2-D data.")
+        if key not in self._children:
+            self._children[key] = Mesh(ax=self._ax,
+                                       data=new_values,
+                                       vmin=self._user_vmin,
+                                       vmax=self._user_vmax,
+                                       norm=self._norm,
+                                       **{
+                                           **{
+                                               'cbar': self._cbar,
+                                           },
+                                           **self._kwargs
+                                       })
+            self._dims.update({
+                "x": {
+                    'dim': new_values.dims[1],
+                    'unit': new_values.meta[new_values.dims[1]].unit
+                },
+                "y": {
+                    'dim': new_values.dims[0],
+                    'unit': new_values.meta[new_values.dims[0]].unit
+                }
+            })
+
+            self._ax.set_xlabel(
+                name_with_unit(var=new_values.meta[self._dims['x']['dim']]))
+            self._ax.set_ylabel(
+                name_with_unit(var=new_values.meta[self._dims['y']['dim']]))
+            if (self._dims['x']['dim'] in self._scale) and (
+                    self._ax.get_xscale() != self._scale[self._dims['x']['dim']]):
+                self.logx()
+            if (self._dims['y']['dim'] in self._scale) and (
+                    self._ax.get_yscale() != self._scale[self._dims['y']['dim']]):
+                self.logy()
+            if not self._ax.get_title():
+                self._ax.set_title(new_values.name)
+
+        else:
+            self._children[key].update(new_values=new_values)
+
+        if draw:
+            self.draw()
