1a70c037d1320db7fa04dc0b86c6dbf64df3b502
Neil Vaytet <neil.vaytet@esss.se>
2022-10-21 15:24:22 +0200

    add tests for fig2d

--- tests/graphics/fig2d_test.py
+++ tests/graphics/fig2d_test.py
@@ -92,3 +92,25 @@ def test_update_on_one_mesh_changes_colors_on_second_mesh():
     a.func = lambda: da1 * 5.0
     a.notify_children('updated a')
     assert not np.allclose(old_b_colors, f.artists[b.id]._mesh.get_facecolors())
+
+
+def test_with_string_coord():
+    strings = ['a', 'b', 'c', 'd', 'e']
+    da = sc.DataArray(data=sc.array(dims=['y', 'x'], values=np.random.random((5, 5))),
+                      coords={
+                          'x': sc.array(dims=['x'], values=strings, unit='s'),
+                          'y': sc.arange('y', 5., unit='m')
+                      })
+    fig = Figure2d(input_node(da))
+    assert [t.get_text() for t in fig.canvas.ax.get_xticklabels()] == strings
+
+
+def test_with_strings_as_bin_edges():
+    strings = ['a', 'b', 'c', 'd', 'e', 'f']
+    da = sc.DataArray(data=sc.array(dims=['y', 'x'], values=np.random.random((5, 5))),
+                      coords={
+                          'x': sc.array(dims=['x'], values=strings, unit='s'),
+                          'y': sc.arange('y', 6., unit='m')
+                      })
+    fig = Figure2d(input_node(da))
+    assert [t.get_text() for t in fig.canvas.ax.get_xticklabels()] == strings
