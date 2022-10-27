4d40dddec67b94fd7ba4ca1a7995b21dd7a0aeb4
Neil Vaytet <neil.vaytet@esss.se>
2022-10-27 19:48:25 +0200

    update box

--- src/plopp/widgets/box.py
+++ src/plopp/widgets/box.py
@@ -1,25 +1,40 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
 
-from ipywidgets import VBox, HBox
+from ipywidgets import VBox, HBox, Widget
 
 
 class Bar:
+    """
+    A simple mixin to provide add and remove helper methods for HBox/VBox containers.
+    """
 
-    def add(self, obj):
+    def add(self, obj: Widget):
+        """
+        Append a widget to the list of children.
+        """
         self.children = list(self.children) + [obj]
 
-    def remove(self, obj):
+    def remove(self, obj: Widget):
+        """
+        Remove a widget from the list of children.
+        """
         children = list(self.children)
         children.remove(obj)
         self.children = children
 
 
 class VBar(VBox, Bar):
+    """
+    Vertical bar container.
+    """
     pass
 
 
 class HBar(HBox, Bar):
+    """
+    Horizontal bar container.
+    """
     pass
 
 
@@ -29,6 +44,12 @@ class Box(VBox):
     item is itself a list, it will be bade into a horizontal row of the underlying
     items, if not, the item will span then entire row.
     Finally, all the rows will be placed inside a vertical box container.
+
+    Parameters
+    ----------
+
+    widgets:
+        List of widgets to place in the box.
     """
 
     def __init__(self, widgets):
