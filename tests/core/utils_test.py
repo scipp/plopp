110eb8b7057c5b3fb8e4127463fff579744cc109
Neil Vaytet <neil.vaytet@esss.se>
2022-11-11 15:55:17 +0100

    add tests

--- tests/core/utils_test.py
+++ tests/core/utils_test.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
 
-from plopp.core.utils import coord_as_bin_edges
+from plopp.core.utils import coord_as_bin_edges, coord_as_midpoints
 import scipp as sc
 
 
@@ -57,3 +57,57 @@ def test_coord_as_bin_edges_non_dimension_coord():
                       })
     result = coord_as_bin_edges(da, key='y', dim='x')
     assert sc.identical(result, sc.linspace('x', 16.5, 21.5, num=6, unit='m'))
+
+
+def test_coord_as_midpoints_edges_input():
+    da = sc.DataArray(data=sc.arange('x', 5., unit='K'),
+                      coords={'x': sc.arange('x', 6., unit='m')})
+    result = coord_as_midpoints(da, key='x')
+    assert sc.identical(result, sc.linspace('x', 0.5, 4.5, num=5, unit='m'))
+
+
+def test_coord_as_midpoints_midpoints_input():
+    x = sc.arange('x', 5., unit='m')
+    da = sc.DataArray(data=sc.arange('x', 5., unit='K'), coords={'x': x})
+    result = coord_as_midpoints(da, key='x')
+    assert sc.identical(result, x)
+
+
+def test_coord_as_midpoints_string_edges_input():
+    strings = ['a', 'b', 'c', 'd', 'e', 'f']
+    da = sc.DataArray(data=sc.arange('x', 5., unit='K'),
+                      coords={'x': sc.array(dims=['x'], values=strings, unit='s')})
+    result = coord_as_midpoints(da, key='x')
+    assert sc.identical(result, sc.linspace('x', 0.5, 4.5, num=5, unit='s'))
+
+
+def test_coord_as_midpoints_string_midpoints_input():
+    strings = ['a', 'b', 'c', 'd', 'e']
+    da = sc.DataArray(data=sc.arange('x', 5., unit='K'),
+                      coords={'x': sc.array(dims=['x'], values=strings, unit='s')})
+    result = coord_as_midpoints(da, key='x')
+    assert sc.identical(result, sc.arange('x', 5.0, unit='s'))
+
+
+def test_coord_as_midpoints_int_edges_input():
+    da = sc.DataArray(data=sc.arange('x', 5., unit='K'),
+                      coords={'x': sc.arange('x', 6, unit='m')})
+    result = coord_as_midpoints(da, key='x')
+    assert sc.identical(result, sc.linspace('x', 0.5, 4.5, num=5, unit='m'))
+
+
+def test_coord_as_midpoints_int_midpoints_input():
+    x = sc.arange('x', 5, unit='m')
+    da = sc.DataArray(data=sc.arange('x', 5., unit='K'), coords={'x': x})
+    result = coord_as_midpoints(da, key='x')
+    assert sc.identical(result, x)
+
+
+def test_coord_as_midpoints_non_dimension_coord():
+    da = sc.DataArray(data=sc.arange('x', 5., unit='K'),
+                      coords={
+                          'x': sc.arange('x', 6., unit='m'),
+                          'y': sc.arange('x', 17., 23., unit='m')
+                      })
+    result = coord_as_midpoints(da, key='y', dim='x')
+    assert sc.identical(result, sc.linspace('x', 17.5, 21.5, num=5, unit='m'))
