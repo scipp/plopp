aa4da2d26953670592a19c386b08d48941236eed
Neil Vaytet <neil.vaytet@esss.se>
2022-12-19 11:56:04 +0100

    remove tests on utils function

--- tests/core/utils_test.py
+++ tests/core/utils_test.py
@@ -58,29 +58,3 @@ def test_coord_as_bin_edges_non_dimension_coord():
                       })
     result = coord_as_bin_edges(da, key='y', dim='x')
     assert sc.identical(result, sc.linspace('x', 16.5, 21.5, num=6, unit='m'))
-
-
-def test_make_compatible_raises_different_dimension():
-    x = sc.arange('x', 6., unit='m')
-    with pytest.raises(sc.DimensionError):
-        make_compatible(x, dim='y', unit='m')
-
-
-def test_make_compatible_raises_incompatible_unit():
-    x = sc.arange('x', 6., unit='m')
-    with pytest.raises(sc.UnitError):
-        make_compatible(x, unit='s')
-
-
-def test_make_compatible_comverts_compatible_unit():
-    x = sc.array(dims=['x'], values=[1., 4., 9.], unit='s')
-    expected = sc.array(dims=['x'], values=[1000., 4000., 9000.], unit='ms')
-    result = make_compatible(x, unit='ms')
-    assert sc.identical(result, expected)
-
-
-def test_make_compatible_comverts_compatible_unit_integers():
-    x = sc.array(dims=['x'], values=[10, 20, 30, 40, 50], unit='cm')
-    expected = sc.array(dims=['x'], values=[0.1, 0.2, 0.3, 0.4, 0.5], unit='m')
-    result = make_compatible(x, unit='m')
-    assert sc.identical(result, expected)
