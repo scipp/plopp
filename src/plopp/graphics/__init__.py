4ac0c2af1d105a34075534f3c983cd7d439e31f2
nvaytet <nvaytet@users.noreply.github.com>
2023-08-02 20:21:06 +0000

    Apply automatic formatting

--- src/plopp/graphics/__init__.py
+++ src/plopp/graphics/__init__.py
@@ -7,7 +7,7 @@ from .basefig import BaseFig
 from .camera import Camera
 from .colormapper import ColorMapper
 from .figure import figure1d, figure2d, figure3d
-from .lineview import LineView
 from .imageview import ImageView
+from .lineview import LineView
 from .scatter3dview import Scatter3dView
 from .tiled import tiled
