2e95f9991fcb63f23127d31b46858a79f392bd4c
Neil Vaytet <neil.vaytet@esss.se>
2023-09-25 14:19:16 +0200

    use x [datetime] instead of no unit

--- src/plopp/core/utils.py
+++ src/plopp/core/utils.py
@@ -130,8 +130,9 @@ def name_with_unit(var: sc.Variable, name: str = None) -> str:
         text = name
     else:
         text = str(var.dims[-1])
-    if (var.unit is not None) and (var.dtype != sc.DType.datetime64):
-        text += (" " if text else "") + f"[{var.unit}]"
+    if var.unit is not None:
+        unit_str = 'datetime' if var.dtype == sc.DType.datetime64 else var.unit
+        text += (" " if text else "") + f"[{unit_str}]"
     return text
 
 
