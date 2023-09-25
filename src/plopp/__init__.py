2fef7bf7343c4a1965be25562cf5edbe2fcd0a66
Neil Vaytet <neil.vaytet@esss.se>
2023-09-25 12:58:15 +0200

    remove old patching functions, remove input_node

--- src/plopp/__init__.py
+++ src/plopp/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 
-# flake8: noqa E402, F401
+# flake8: noqa E402
 
 import importlib.metadata
 
@@ -15,36 +15,9 @@ from .backends.manager import BackendManager
 backends = BackendManager()
 
 from . import data
-from .core import Node, View, input_node, node, show_graph, widget_node
+from .core import Node, View, node, show_graph, widget_node
 from .graphics import Camera, figure1d, figure2d, figure3d, tiled
 from .plotting import inspector, plot, scatter3d, slicer, superplot
-
-
-def patch_scipp():
-    """
-    Running this replaces the ``plot`` function from Scipp with the plopp ``plot``
-    wrapper. This patches the ``Variable``, ``DataArray``, ``Dataset`` classes,
-    as well as the main ``plot`` function in the Scipp module.
-    """
-    import scipp as sc
-
-    setattr(sc.Variable, 'plot', plot)
-    setattr(sc.DataArray, 'plot', plot)
-    setattr(sc.Dataset, 'plot', plot)
-    setattr(sc, 'plot', plot)
-
-
-def unpatch_scipp():
-    """
-    Running this reverts the patching operation in :func:`patch_scipp`.
-    """
-    import scipp as sc
-    from scipp.plotting import plot as pl
-
-    setattr(sc.Variable, 'plot', pl)
-    setattr(sc.DataArray, 'plot', pl)
-    setattr(sc.Dataset, 'plot', pl)
-    setattr(sc, 'plot', pl)
 
 
 def show():
@@ -57,3 +30,25 @@ def show():
     import matplotlib.pyplot as plt
 
     plt.show()
+
+
+__all__ = [
+    'Camera',
+    'Node',
+    'View',
+    'backends',
+    'data',
+    'figure1d',
+    'figure2d',
+    'figure3d',
+    'inspector',
+    'node',
+    'plot',
+    'scatter3d',
+    'show',
+    'show_graph',
+    'slicer',
+    'superplot',
+    'tiled',
+    'widget_node',
+]
