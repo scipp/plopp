71c80a8466d2e4d4bc4dd74890572829195746e0
Neil Vaytet <neil.vaytet@esss.se>
2024-01-12 13:14:36 +0100

    remove unused type

--- src/plopp/core/limits.py
+++ src/plopp/core/limits.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 
-from typing import Literal, Optional, Tuple
+from typing import Literal, Tuple
 
 import numpy as np
 import scipp as sc
