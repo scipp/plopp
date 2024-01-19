357eedb59dc0cda2fa9274edf67be583e0ea3d34
Neil Vaytet <neil.vaytet@esss.se>
2024-01-19 16:00:02 +0100

    add tests

--- tests/backends/plotly/plotly_figure_test.py
+++ tests/backends/plotly/plotly_figure_test.py
@@ -3,10 +3,14 @@
 
 import pytest
 
+import numpy as np
+import scipp as sc
+
 import plopp as pp
 from plopp.backends.plotly.figure import Figure
 from plopp.data.testing import data_array
 from plopp.graphics.lineview import LineView
+from plopp.graphics.polarview import PolarLineView
 
 pytest.importorskip("plotly")
 
@@ -28,3 +32,32 @@ def test_logy_1d_toolbar_button():
     da = data_array(ndim=1)
     fig = Figure(LineView, pp.Node(da), norm='log')
     assert fig.toolbar['logy'].value
+
+
+def test_polar_figure():
+    N = 150
+    r = sc.linspace('theta', 0, 10, N, unit='m')
+    theta = sc.linspace('theta', 0, 12, N, unit='rad')
+    da = sc.DataArray(data=r, coords={'theta': theta})
+    f = Figure(PolarLineView, pp.Node(da))
+    assert np.array_equal(f.canvas.xrange, (0, 2 * np.pi))
+
+
+def test_polar_figure_set_xrange():
+    N = 150
+    r = sc.linspace('theta', 0, 10, N, unit='m')
+    theta = sc.linspace('theta', 0, 12, N, unit='rad')
+    da = sc.DataArray(data=r, coords={'theta': theta})
+    f = Figure(PolarLineView, pp.Node(da))
+    f.canvas.xrange = (0.5, 11.0)
+    assert np.array_equal(f.canvas.xrange, (0.5, 11.0))
+
+
+def test_polar_figure_set_yrange():
+    N = 150
+    r = sc.linspace('theta', 0, 10, N, unit='m')
+    theta = sc.linspace('theta', 0, 12, N, unit='rad')
+    da = sc.DataArray(data=r, coords={'theta': theta})
+    f = Figure(PolarLineView, pp.Node(da))
+    f.canvas.yrange = (0.5, 8.0)
+    assert np.array_equal(f.canvas.yrange, (0.5, 8.0))
--- tests/plotting/polar_plot_test.py
+++ tests/plotting/polar_plot_test.py
@@ -1,15 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 
-import os
-import tempfile
-
 import numpy as np
-import pytest
 import scipp as sc
 
 import plopp as pp
-from plopp.data.testing import data_array, dataset
+from plopp.data.testing import data_array
 
 
 def test_polar_plot_1d_wrap_around_2_pi():
@@ -18,7 +14,7 @@ def test_polar_plot_1d_wrap_around_2_pi():
     theta = sc.linspace('theta', 0, 12, N, unit='rad')
     da = sc.DataArray(data=r, coords={'theta': theta})
     f = pp.polar(da)
-    assert f.canvas.xrange == (0, 2 * np.pi)
+    assert np.array_equal(f.canvas.xrange, (0, 2 * np.pi))
 
 
 def test_polar_plot_1d_small_range():
@@ -31,12 +27,29 @@ def test_polar_plot_1d_small_range():
     assert f.canvas.xmax < 2 * np.pi
 
 
+def test_polar_plot_1d_variances():
+    N = 50
+    r = sc.linspace('theta', 0, 10, N, unit='m')
+    r.variances = r.values
+    theta = sc.linspace('theta', 0, 4, N, unit='rad')
+    da = sc.DataArray(data=r, coords={'theta': theta})
+    pp.polar(da)
+
+
+def test_polar_plot_bin_edges():
+    N = 50
+    r = sc.linspace('theta', 0, 10, N, unit='m')
+    theta = sc.linspace('theta', 0, 4, N + 1, unit='rad')
+    da = sc.DataArray(data=r, coords={'theta': theta})
+    pp.polar(da)
+
+
 def test_polar_plot_2d_2_pi():
     da = data_array(ndim=2, binedges=True, unit='K')
     xdim = da.dims[-1]
     da.coords[xdim] = sc.linspace(xdim, 0, 2 * np.pi, da.sizes[xdim] + 1, unit='rad')
     f = pp.polar(da)
-    assert f.canvas.xrange == (0, 2 * np.pi)
+    assert np.array_equal(f.canvas.xrange, (0, 2 * np.pi))
 
 
 def test_polar_plot_2d_small_range():
@@ -44,4 +57,4 @@ def test_polar_plot_2d_small_range():
     xdim = da.dims[-1]
     da.coords[xdim] = sc.linspace(xdim, 0, 1.3 * np.pi, da.sizes[xdim] + 1, unit='rad')
     f = pp.polar(da)
-    assert f.canvas.xrange == (0, 1.3 * np.pi)
+    assert np.array_equal(f.canvas.xrange, (0, 1.3 * np.pi))
