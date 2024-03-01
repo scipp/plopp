abd8608de75f1fb5ddc89d05a4d0faf8be34371c
Neil Vaytet <neil.vaytet@esss.se>
2024-03-01 16:31:49 +0100

    add scatterview tests

--- tests/plotting/scatter_test.py
+++ tests/plotting/scatter_test.py
@@ -6,7 +6,7 @@ import pytest
 import scipp as sc
 
 import plopp as pp
-from plopp.data.testing import scatter as scatter_data
+from plopp.data.testing import data_array, scatter as scatter_data
 
 
 def test_scatter_simple():
@@ -42,6 +42,12 @@ def test_scatter_with_colorbar():
     assert scat._view.colormapper is not None
 
 
+def test_scatter_with_cmap():
+    name = 'magma'
+    scat = pp.scatter(scatter_data(), cbar=True, cmap=name)
+    assert scat._view.colormapper.cmap.name == name
+
+
 def test_scatter_with_size():
     a = scatter_data()
     a.coords['s'] = sc.abs(a.coords['x']) * 5
@@ -58,3 +64,25 @@ def test_scatter_with_masks():
     a = scatter_data()
     a.masks['m'] = a.coords['x'] > sc.scalar(10, unit='m')
     pp.scatter(a)
+
+
+def test_scatter_flattens_2d_data():
+    pp.scatter(data_array(ndim=2), x='xx', y='yy', cbar=True)
+
+
+def test_scatter_with_norm():
+    a = scatter_data()
+    scat = pp.scatter(a, cbar=True, norm='linear')
+    assert scat._view.colormapper.norm == 'linear'
+    scat = pp.scatter(a, cbar=True, norm='log')
+    assert scat._view.colormapper.norm == 'log'
+
+
+def test_scatter_log_axes():
+    a = scatter_data()
+    scat = pp.scatter(a)
+    assert scat._view.canvas.xscale == 'linear'
+    assert scat._view.canvas.yscale == 'linear'
+    scat = pp.scatter(a, scale={'x': 'log', 'y': 'log'})
+    assert scat._view.canvas.xscale == 'log'
+    assert scat._view.canvas.yscale == 'log'
