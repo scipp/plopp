20f692fd7b9d770efdee27f0e38fc3e7b9e6d0a5
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2024-05-17 09:59:01 +0200

    Copier update

--- docs/conf.py
+++ docs/conf.py
@@ -9,9 +9,9 @@ from importlib.metadata import version as get_version
 sys.path.insert(0, os.path.abspath('.'))
 
 # General information about the project.
-project = u'Plopp'
-copyright = u'2024 Scipp contributors'
-author = u'Scipp contributors'
+project = 'Plopp'
+copyright = '2024 Scipp contributors'
+author = 'Scipp contributors'
 
 html_show_sourcelink = True
 
