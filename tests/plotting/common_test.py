scipp/plopp

commit 649064ef4c3c40a859b15acba74ffdf3d22361fe

fix plotting with left over bin edges

src/plopp/plotting/common.py
@@ -98,6 +98,10 @@ def to_data_array(
     for dim, size in out.sizes.items():
         if dim not in out.coords:
             out.coords[dim] = sc.arange(dim, size, unit=None)
+    for name in out.coords:
+        other_dims = [dim for dim in out.coords[name].dims if dim not in out.dims]
+        for dim in other_dims:
+            out.coords[name] = out.coords[name].mean(dim)
     return out
 
 

tests/plotting/plot_2d_test.py
@@ -333,3 +333,9 @@ def test_figure_has_only_unit_on_colorbar_for_multiple_images(linspace):
     assert str(b.unit) in ylabel
     assert a.name not in ylabel
     assert b.name not in ylabel
+
+
+def test_plot_with_bin_edges_left_over_from_slicing():
+    da = data_array(ndim=2, binedges=True)
+    f = da.fold(dim='xx', sizes={'xx': 25, 'pulse': 2})
+    f['pulse', 0].plot()

commit 067b4ab471a8d9727e07912e0cccf9231f4bcc57

Drop unused coords
AFAIK, there is no need to keep all coords around. And this caused a problem in Scipp where the averaging introduced in 649064ef4c3c40a859b15acba74ffdf3d22361fe fails for a datetime coord.

src/plopp/plotting/common.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
 
 import warnings
-from collections.abc import Callable
+from collections.abc import Callable, Iterable
 from typing import Any
 
 import numpy as np
@@ -76,8 +76,30 @@ def to_variable(obj) -> sc.Variable:
     return out
 
 
+def _ensure_data_array_coords(
+    da: sc.DataArray, coords: list[str] | None
+) -> sc.DataArray:
+    if coords is None:
+        coords = list(da.dims)
+    elif missing := set(coords) - set(da.coords.keys()):
+        raise ValueError(f"Specified coords do not exist: {missing}")
+
+    # Remove unused coords
+    da = da.drop_coords(list(set(da.coords) - set(coords)))
+    # Assign dim coords where missing. The above checks ensure that all missing
+    # coords are dim coords, i.e., that `name in out.dims`.
+    da = da.assign_coords(
+        {
+            name: sc.arange(name, da.sizes[name], unit=None)
+            for name in set(coords) - set(da.coords)
+        }
+    )
+    return da
+
+
 def to_data_array(
     obj: Plottable | list,
+    coords: list[str] | None = None,
 ) -> sc.DataArray:
     """
     Convert an input to a DataArray, potentially adding fake coordinates if they are
@@ -87,6 +109,8 @@ def to_data_array(
     ----------
     obj:
         The input object to be converted.
+    coords:
+        If supplied, use these coords instead of the input's dimension coordinates.
     """
     out = _maybe_to_variable(obj)
     if isinstance(out, sc.Variable):
@@ -94,15 +118,13 @@ def to_data_array(
     out = from_compatible_lib(out)
     if not isinstance(out, sc.DataArray):
         raise TypeError(f"Cannot convert input of type {type(obj)} to a DataArray.")
-    out = out.copy(deep=False)
-    for dim, size in out.sizes.items():
-        if dim not in out.coords:
-            out.coords[dim] = sc.arange(dim, size, unit=None)
-        if not out.coords[dim].dims:
+    out = _ensure_data_array_coords(out, coords)
+    for name, coord in out.coords.items():
+        if not coord.dims:
             raise ValueError(
                 "Input data cannot be plotted: it has a scalar coordinate along "
-                f"dimension {dim}. Consider dropping this coordinate before plotting. "
-                f"Use ``data.drop_coords('{dim}').plot()``."
+                f"dimension {name}. Consider dropping this coordinate before plotting. "
+                f"Use ``data.drop_coords('{name}').plot()``."
             )
     for name in out.coords:
         other_dims = [dim for dim in out.coords[name].dims if dim not in out.dims]
@@ -171,7 +193,7 @@ def preprocess(
     obj: Plottable | list,
     name: str | None = None,
     ignore_size: bool = False,
-    coords: list[str] | None = None,
+    coords: Iterable[str] | str | None = None,
 ) -> sc.DataArray:
     """
     Pre-process input data for plotting.
@@ -192,7 +214,12 @@ def preprocess(
     coords:
         If supplied, use these coords instead of the input's dimension coordinates.
     """
-    out = to_data_array(obj)
+    if isinstance(coords, str):
+        coords = [coords]
+    elif coords is not None:
+        coords = list(coords)
+
+    out = to_data_array(obj, coords)
     check_not_binned(out)
     check_allowed_dtypes(out)
     if name is not None:

tests/plotting/common_test.py
@@ -40,12 +40,12 @@ def test_preprocess_no_warning_if_dtype_cannot_be_sorted():
         dims=['xx'], values=np.random.random((da.sizes['xx'], 3))
     )
     out = preprocess(da)  # no warning should be emitted
-    assert 'vecs' in out.coords
+    assert 'vecs' not in out.coords  # coord doesn't participate in plot
     da.coords['strings'] = sc.array(
         dims=['xx'], values=list('ba' * (da.sizes['xx'] // 2))
     )
     out = preprocess(da)  # no warning should be emitted
-    assert 'strings' in out.coords
+    assert 'strings' not in out.coords  # coord doesn't participate in plot
 
 
 @pytest.mark.parametrize(
@@ -70,3 +70,19 @@ def test_preprocess_raises_for_unsupported_dtype(dtype_and_shape):
         match=f'The input has dtype {dtype} which is not supported by Plopp',
     ):
         preprocess(v)
+
+
+def test_preprocess_drops_coords_that_are_not_plotted_dim_coords():
+    da = data_array(ndim=2)
+    da.coords['xx2'] = 7.5 * da.coords['xx']
+    da.coords['yy2'] = 3.3 * da.coords['yy']
+    out = preprocess(da)
+    assert set(out.coords) == {'xx', 'yy'}
+
+
+def test_preprocess_drops_coords_that_are_not_plotted_custom_coords():
+    da = data_array(ndim=2)
+    da.coords['xx2'] = 7.5 * da.coords['xx']
+    da.coords['yy2'] = 3.3 * da.coords['yy']
+    out = preprocess(da, coords=['yy2', 'xx'])
+    assert set(out.coords) == {'xx', 'yy2'}

tests/plotting/plot_1d_test.py
@@ -414,6 +414,16 @@ def test_plot_1d_datetime_length_1():
     pp.plot(da)
 
 
+def test_plot_1d_extra_datetime_coord_binedges():
+    da = data_array(ndim=1)
+    dim = da.dim
+    size = da.sizes[dim]
+    da.coords['times'] = (sc.arange('t', 2 * size, unit='s') + sc.epoch(unit='s')).fold(
+        dim='t', sizes={dim: size, 't': 2}
+    )
+    pp.plot(da)
+
+
 def test_plot_1d_data_with_errorbars():
     da = data_array(ndim=1, variances=True)
     p = da.plot()