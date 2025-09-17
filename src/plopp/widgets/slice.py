1d91f0045867d27a6f483972d88859fbba3c1d33
Neil Vaytet <neil.vaytet@ess.eu>
2025-09-17 14:33:17 +0200

    make slider width constant, independent of description length

--- src/plopp/widgets/slice.py
+++ src/plopp/widgets/slice.py
@@ -27,7 +27,6 @@ class DimSlicer(ipw.VBox):
             )
         widget_args = {
             'step': 1,
-            'description': dim,
             'min': 0,
             'max': size - 1,
             'value': (size - 1) // 2 if slider_constr is ipw.IntSlider else None,
@@ -36,6 +35,7 @@ class DimSlicer(ipw.VBox):
             'layout': {"width": "200px", "margin": "0px 0px 0px 10px"},
             'style': {'description_width': 'initial'},
         }
+        self.dim_label = ipw.Label(value=dim)
         self.slider = slider_constr(**widget_args)
         self.continuous_update = ipw.Checkbox(
             value=True,
@@ -50,7 +50,7 @@ class DimSlicer(ipw.VBox):
             (self.continuous_update, 'value'), (self.slider, 'continuous_update')
         )
 
-        children = [self.slider, self.continuous_update, self.label]
+        children = [self.dim_label, self.slider, self.continuous_update, self.label]
         if enable_player:
             self.player = ipw.Play(
                 value=self.slider.value,
