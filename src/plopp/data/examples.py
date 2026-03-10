0fd750f77f46457feb95be195e933ad073e3c2ff
Neil Vaytet <neil.vaytet@ess.eu>
2026-03-10 17:54:06 +0100

    use file repo on github instead of dmsc

--- src/plopp/data/examples.py
+++ src/plopp/data/examples.py
@@ -16,7 +16,7 @@ def _make_pooch():
     return pooch.create(
         path=pooch.os_cache('plopp'),
         env='PLOPP_DATA_DIR',
-        base_url='https://public.esss.dk/groups/scipp/plopp/{version}/',
+        base_url='https://github.com/scipp/plopp-datafiles/raw/refs/heads/main/{version}/',
         version=_version,
         registry={
             'nyc_taxi_data.h5': 'md5:fc0867ec061e4ac0cbe5975a665a0eea',
