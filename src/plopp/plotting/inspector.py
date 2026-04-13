c0adde9915428305c8169f129a4880148992c85b
Neil Vaytet <neil.vaytet@ess.eu>
2026-04-13 10:12:05 +0200

    document selection behaviour

--- src/plopp/plotting/inspector.py
+++ src/plopp/plotting/inspector.py
@@ -202,6 +202,13 @@ def inspector(
     Almost all the arguments for plot customization apply to the two-dimensional image
     (unless specified).
 
+    In rectangle mode, if one of the edges of the rectangle lies exactly on a bin edge
+    between two data points, the selected region does not include the data on the
+    outside of the rectangle, even though the edge is touching the rectangle.
+
+    In polygon mode, only data whose bin centers are inside the polygon are included in
+    the selected region.
+
     Parameters
     ----------
     obj:
