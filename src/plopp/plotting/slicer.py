4b2d8814295f74db97ceb69234d6490b3c676bc7
Neil Vaytet <neil.vaytet@ess.eu>
2026-04-13 22:44:01 +0200

    make slicer more reusable by better splitting the slicing and the code that makes the figure

--- src/plopp/plotting/slicer.py
+++ src/plopp/plotting/slicer.py
@@ -160,24 +160,24 @@ class Slicer:
             for node in self.slice_nodes
         ]
 
-        args = categorize_args(**kwargs)
+        # args = categorize_args(**kwargs)
 
-        ndims = len(keep)
-        if ndims == 1:
-            make_figure = partial(linefigure, **args['1d'])
-        elif ndims == 2:
-            if len(self.slice_nodes) > 1:
-                raise_multiple_inputs_for_2d_plot_error(origin='slicer')
-            make_figure = partial(imagefigure, **args['2d'])
-        else:
-            raise ValueError(
-                f'Slicer plot: the number of dims to be kept must be 1 or 2, '
-                f'but {ndims} were requested.'
-            )
+        # ndims = len(keep)
+        # if ndims == 1:
+        #     make_figure = partial(linefigure, **args['1d'])
+        # elif ndims == 2:
+        #     if len(self.slice_nodes) > 1:
+        #         raise_multiple_inputs_for_2d_plot_error(origin='slicer')
+        #     make_figure = partial(imagefigure, **args['2d'])
+        # else:
+        #     raise ValueError(
+        #         f'Slicer plot: the number of dims to be kept must be 1 or 2, '
+        #         f'but {ndims} were requested.'
+        #     )
 
-        self.figure = make_figure(*self.reduce_nodes)
-        require_interactive_figure(self.figure, 'slicer')
-        self.figure.bottom_bar.add(self.slider)
+        # self.figure = make_figure(*self.reduce_nodes)
+        # require_interactive_figure(self.figure, 'slicer')
+        # self.figure.bottom_bar.add(self.slider)
 
 
 def slicer(
@@ -311,7 +311,7 @@ def slicer(
     **kwargs:
         Additional arguments forwarded to the underlying plotting library.
     """
-    return Slicer(
+    sl = Slicer(
         obj,
         keep=keep,
         aspect=aspect,
@@ -347,4 +347,24 @@ def slicer(
         ymax=ymax,
         ymin=ymin,
         **kwargs,
-    ).figure
+    )
+
+    args = categorize_args(**kwargs)
+
+    ndims = len(keep)
+    if ndims == 1:
+        make_figure = partial(linefigure, **args['1d'])
+    elif ndims == 2:
+        if len(sl.slice_nodes) > 1:
+            raise_multiple_inputs_for_2d_plot_error(origin='slicer')
+        make_figure = partial(imagefigure, **args['2d'])
+    else:
+        raise ValueError(
+            f'Slicer plot: the number of dims to be kept must be 1 or 2, '
+            f'but {ndims} were requested.'
+        )
+
+    figure = make_figure(*sl.reduce_nodes)
+    require_interactive_figure(figure, 'slicer')
+    figure.bottom_bar.add(sl.slider)
+    return figure
