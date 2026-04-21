1f4f7c305e2aea2c17fc7e7b2c178b7c37d8cb1c
Neil Vaytet <neil.vaytet@ess.eu>
2026-04-21 10:53:15 +0200

    Apply copier update April 2026

--- src/plopp/__init__.py
+++ src/plopp/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
+# Copyright (c) 2026 Scipp contributors (https://github.com/scipp)
 # ruff: noqa: RUF100, E402, F401, I
 
 import importlib.metadata
