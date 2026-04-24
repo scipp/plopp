59c56e823a5792722ba5501ffb5ccf2817af06e5
Neil Vaytet <neil.vaytet@ess.eu>
2026-04-24 17:25:37 +0200

    add files to activate backend on import

---
+++ src/plopp/plotly.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2026 Scipp contributors (https://github.com/scipp)
+
+import sys
+
+import plopp as _plopp
+
+_plopp.backends['2d'] = 'plotly'
+
+sys.modules[__name__] = _plopp
